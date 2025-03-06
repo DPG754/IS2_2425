cdf5758b0c1d73eff3b9de6b03329a90729c2f46
Andres3187 <andresrn2016@gmail.com>
2025-03-06 13:22:55 +0100

    motocicleta terminado

--- Practica3/ProyectoMavenCommon/src/main/java/Motocicleta.java
+++ Practica3/ProyectoMavenCommon/src/main/java/Motocicleta.java
@@ -27,6 +27,18 @@ public class Motocicleta extends Vehiculo {
 		//TO-DO
 		double pImpuesto = 0;
 		
+		if (this.cilindrada <= 125) {
+			pImpuesto = 8;
+		} else if (this.cilindrada <= 250) {
+			pImpuesto = 15;
+		} else if (this.cilindrada <= 500) {
+			pImpuesto = 30;
+		} else if (this.cilindrada <= 1000) {
+			pImpuesto = 60; 
+		} else {
+			pImpuesto = 120;
+		}
+		
 		return pImpuesto;
 	}
 
